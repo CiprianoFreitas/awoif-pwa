4c55ce996695b621225f2b6e4fbb3ced144cceda
Cipriano Freitas <casf1992@gmail.com>
2017-02-27 19:45:38 +0000

    :sparkles: added basic service worker to the app - fixes #1

--- App/boot.tsx
+++ App/boot.tsx
@@ -4,7 +4,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Hello } from "./Hello";
 
-ReactDOM.render(
-    <Hello />,
-    document.getElementById("app")
-);
+declare var require: any;
+
+// Client render
+if (typeof document !== 'undefined') {
+    const register = require("serviceworker-loader!../service-worker.js");
+    register({ scope: './' })
+        .then(() => console.log('It worked!'))
+        .catch((err : any) => console.log('Didn\'t work', err))
+
+    ReactDOM.render(
+        <Hello />,
+        document.getElementById("app")
+    );
+}
