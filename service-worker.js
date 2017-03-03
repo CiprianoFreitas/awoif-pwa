b061d1b4d920227ad63ade1d3fde939422a98430
Cipriano Freitas <casf1992@gmail.com>
2017-03-03 15:04:15 +0000

    :ok_hand: added service worker versioning

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+var version = 'v1::';
+
 var filesToCache = [
   '/',
   'main.js'
@@ -26,25 +28,11 @@ self.addEventListener("fetch", function(event) {
     console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
     return;
   }
-  /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
-     Fulfillment result will be used as the response, and rejection will end in a
-     HTTP response indicating failure.
-  */
+
   event.respondWith(
     caches
-      /* This method returns a promise that resolves to a cache entry matching
-         the request. Once the promise is settled, we can then provide a response
-         to the fetch request.
-      */
       .match(event.request)
       .then(function(cached) {
-        /* Even if the response is in our cache, we go to the network as well.
-           This pattern is known for producing "eventually fresh" responses,
-           where we return cached responses immediately, and meanwhile pull
-           a network response and store that in the cache.
-           Read more:
-           https://ponyfoo.com/articles/progressive-networking-serviceworker
-        */
         var networked = fetch(event.request)
           // We handle the network request with success and failure scenarios.
           .then(fetchedFromNetwork, unableToResolve)
@@ -69,10 +57,6 @@ self.addEventListener("fetch", function(event) {
             // We open a cache to store the response for this request.
             .open(version + 'pages')
             .then(function add(cache) {
-              /* We store the response for this request. It'll later become
-                 available to caches.match(event.request) calls, when looking
-                 for cached responses.
-              */
               cache.put(event.request, cacheCopy);
             })
             .then(function() {
@@ -83,27 +67,9 @@ self.addEventListener("fetch", function(event) {
           return response;
         }
 
-        /* When this method is called, it means we were unable to produce a response
-           from either the cache or the network. This is our opportunity to produce
-           a meaningful response even when all else fails. It's the last chance, so
-           you probably want to display a "Service Unavailable" view or a generic
-           error response.
-        */
         function unableToResolve () {
-          /* There's a couple of things we can do here.
-             - Test the Accept header and then return one of the `offlineFundamentals`
-               e.g: `return caches.match('/some/cached/image.png')`
-             - You should also consider the origin. It's easier to decide what
-               "unavailable" means for requests against your origins than for requests
-               against a third party, such as an ad provider
-             - Generate a Response programmaticaly, as shown below, and return that
-          */
-
           console.log('WORKER: fetch request failed in both cache and network.');
 
-          /* Here we're creating a response programmatically. The first parameter is the
-             response body, and the second one defines the options for the response.
-          */
           return new Response('<h1>Service Unavailable</h1>', {
             status: 503,
             statusText: 'Service Unavailable',
@@ -112,6 +78,37 @@ self.addEventListener("fetch", function(event) {
             })
           });
         }
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  console.log('WORKER: activate event in progress.');
+
+  event.waitUntil(
+    caches
+      /* This method returns a promise which will resolve to an array of available
+         cache keys.
+      */
+      .keys()
+      .then(function (keys) {
+        // We return a promise that settles when all outdated caches are deleted.
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              // Filter by keys that don't start with the latest version prefix.
+              return !key.startsWith(version);
+            })
+            .map(function (key) {
+              /* Return a promise that's fulfilled
+                 when each outdated cache is deleted.
+              */
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function() {
+        console.log('WORKER: activate completed.');
       })
   );
 });
