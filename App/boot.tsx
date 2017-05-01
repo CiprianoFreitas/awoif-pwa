a1b5ba75d8e36b0f73fa26669f54cac9ce1bda73
Cipriano Freitas <casf1992@gmail.com>
2017-05-01 21:50:26 +0100

    :bug: fixed bug where app didnt work on browsers which dont support service workers

--- App/boot.tsx
+++ App/boot.tsx
@@ -8,11 +8,12 @@ import { App } from "./Components/App";
 declare var require: any;
 // Client render
 if (typeof document !== 'undefined') {
-    const register = require("serviceworker-loader!../service-worker.js");
-    register({ scope: './' })
-        .then(() => console.log('Service worker registered'))
-        .catch((err: any) => console.log('Service worker failed to register', err))
-
+    if ('serviceWorker' in navigator) {
+        const register = require("serviceworker-loader!../service-worker.js");
+        register({ scope: './' })
+            .then(() => console.log('Service worker registered'))
+            .catch((err: any) => console.log('Service worker failed to register', err))
+    }
     ReactDOM.render(
         <App />,
         document.getElementById("app")
