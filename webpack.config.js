5b6da6a486048e2cc97e5c30c17b9f6ddda850dd
Cipriano Freitas <casf1992@gmail.com>
2017-05-09 19:04:15 +0100

    :wrench: added sourcemap files - fix #11

--- webpack.config.js
+++ webpack.config.js
@@ -32,14 +32,18 @@ module.exports = (env) => {
                     use: 'file-loader?name=[path][name].[ext]'
                 }
             ]
-        }
-        // ,
-        // plugins: [
-        //     new webpack.optimize.UglifyJsPlugin({
-        //         compress: {
-        //             warnings: false // https://github.com/webpack/webpack/issues/1496
-        //         }
-        //     })
-        // ]
+        },
+        plugins: isDevBuild ? [
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false // https://github.com/webpack/webpack/issues/1496
+                }
+            })
+        ]
     }];
 };
